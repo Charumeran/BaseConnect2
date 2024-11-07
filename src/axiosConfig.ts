266faa2a6085147fdffc2b80df66c1fb0279f545
Charumeran <s.ryosei06@gmail.com>
2024-11-07 19:14:40 +0900

    Route Change URL for GitHub

--- src/axiosConfig.ts
+++ src/axiosConfig.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL, // Rails API のベース URL
+  baseURL: process.env.NODE_ENV === 'production'
+  ? 'https://charumeran-e88af2d8bf0b.herokuapp.com/api/v1'
+  : 'http://localhost:3000/api/v1', // Rails API のベース URL
   headers: {
     'Content-Type' : 'application/json ',
   },
