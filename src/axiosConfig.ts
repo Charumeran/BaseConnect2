f0dfa9b8610a994af2daf0d55a16636db0a867c5
Charumeran <s.ryosei06@gmail.com>
2024-11-08 16:32:23 +0900

    add url

--- src/axiosConfig.ts
+++ src/axiosConfig.ts
@@ -2,10 +2,10 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: process.env.NODE_ENV === 'production'
-  ? 'https://charumeran-e88af2d8bf0b.herokuapp.com/api/v1'
-  : 'http://localhost:3000/api/v1', // Rails API のベース URL
+    ? 'https://charumeran-e88af2d8bf0b.herokuapp.com/api/v1'
+    : 'http://localhost:3000/api/v1', // Rails API のベース URL
   headers: {
-    'Content-Type' : 'application/json ',
+    'Content-Type': 'application/json', // スペースを削除
   },
 });
 
